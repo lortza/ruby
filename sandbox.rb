56a3a1dbc856d00b1c477dd1120256d9162ee2db
anne <richardson.ae@gmail.com>
2015-05-25 15:35:46 -0500

    created new tarot card reading app for terminal

--- sandbox.rb
+++ sandbox.rb
@@ -1,33 +1,35 @@
 
-title = "Bienvenidos al Cajón de Arena!"
+title = "Todos Bienvenidos al Cajón de Arena!"
 maxlength = title.length.to_i + 6
 puts "\n"
 puts "~" * maxlength
-puts title.upcase.center(maxlength)
+puts title.center(maxlength)
 puts "~" * maxlength
 puts "\n"
 
 
 #------CODES-THAT-WILL-BLOW-YOUR-MIND---------
 
-class Die
-  def roll
-    1 + rand(6)
-  end#def
-end #class
+# class Die
+#   def roll
+#     1 + rand(6)
+#   end#def
+# end #class
 
-die1 = Die.new
-die2 = Die.new
-die3 = Die.new
+# die1 = Die.new
+# die2 = Die.new
+# die3 = Die.new
 
-dice = [die1, die2]
+# dice = [die1, die2]
 
-scorepad = 0
+# scorepad = 0
 
-dice.each do |x|
-  x = x.roll.to_i #rolls each die and turns it into an integer
-  puts x #displays the value of the roll
-  scorepad = x + scorepad #adds each roll to the scorepad and redefines the scorepad as having that roll in it
-end# each do
-  puts "---------"
-  puts scorepad
+# dice.each do |x|
+#   x = x.roll.to_i #rolls each die and turns it into an integer
+#   puts x #displays the value of the roll
+#   scorepad = x + scorepad #adds each roll to the scorepad and redefines the scorepad as having that roll in it
+# end# each do
+#   puts "---------"
+#   puts scorepad
+
+
